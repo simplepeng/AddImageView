ca9813537fd59248bc6ef1a42c347ae54c5faf06
chenpeng <383559698@qq.com>
2021-05-25 13:56:16 +0800

    更新ItemGap

--- add_image_view/src/main/java/me/simple/view/ItemGapDecoration.kt
+++ add_image_view/src/main/java/me/simple/view/ItemGapDecoration.kt
@@ -5,7 +5,9 @@ import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class ItemGapDecoration(val itemGap: Int) : RecyclerView.ItemDecoration() {
+class ItemGapDecoration(
+    private val itemGap: Int
+) : RecyclerView.ItemDecoration() {
 
     override fun getItemOffsets(
         outRect: Rect,
@@ -13,10 +15,11 @@ class ItemGapDecoration(val itemGap: Int) : RecyclerView.ItemDecoration() {
         parent: RecyclerView,
         state: RecyclerView.State
     ) {
-        val layoutManager = parent.layoutManager as? GridLayoutManager ?: return
-        val adapter = parent.adapter ?: return
-        val position = parent.getChildAdapterPosition(view)
-        val spanCount = layoutManager.spanCount
+//        val layoutManager = parent.layoutManager as? GridLayoutManager ?: return
+//        val adapter = parent.adapter ?: return
+//        val position = parent.getChildAdapterPosition(view)
+//        val spanCount = layoutManager.spanCount
+
 
         outRect.set(itemGap, itemGap, itemGap, itemGap)
 
@@ -27,19 +30,26 @@ class ItemGapDecoration(val itemGap: Int) : RecyclerView.ItemDecoration() {
 //
 //        when {
 //            position % spanCount == 0 -> {//left
-//                left = itemGap
+//                left = 0
+//                right = itemGap
 //            }
 //            (position + 1) % spanCount == 0 -> {//right
-//                right = itemGap
-//            }
-//            else -> {
 //                left = itemGap
 //                right = itemGap
+//            }
+//            else -> {//center
+//                left = itemGap / 2
+//                right = 0
 //            }
 //        }
 //
 //        bottom = itemGap
 //
 //        outRect.set(left, top, right, bottom)
+
+//        val column = position % spanCount
+//        outRect.left = column * itemGap / spanCount
+//        outRect.right = itemGap - (column + 1) * itemGap / spanCount
+//        outRect.bottom = itemGap
     }
 }
